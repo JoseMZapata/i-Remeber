87d942a4cd8fa942d84c35a9b0d358548371d077
JoseMZapata <josemanuelzapaatarangel@gmail.com>
2025-03-13 12:58:25 -0600

    UI Home y EdList

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
+import { importProvidersFrom } from '@angular/core';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { Storage } from '@ionic/storage-angular'; // Asegurar que Storage esté importado
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    importProvidersFrom(IonicStorageModule.forRoot()), // Incluir el módulo de almacenamiento
+    Storage // Proveer el servicio de Storage
   ],
 });
